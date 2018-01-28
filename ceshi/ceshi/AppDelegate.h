de4b98c61cb1790a13e70c258aa326a2d2862ed4
dawn <ios@ipv6.local>
2018-01-28 15:03:06 +0800

    第一次提交

---
+++ ceshi/ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by dawn on 2018/1/28.
+//  Copyright © 2018年 dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by dawn on 2018/1/28.
+//  Copyright © 2018年 dawn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
